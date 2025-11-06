9fe17574138de466121a2f63ec5de18f5fe3a2c9
ZnoX-os <wgerber27@gmail.com>
2025-11-06 11:49:12 +0200

    Update EmailJS service and template IDs

--- Safari Stop Nonsense/Safari Stop Nonsense/js/config.js
+++ Safari Stop Nonsense/Safari Stop Nonsense/js/config.js
@@ -1,5 +1,5 @@
 // Configuration for EmailJS
 // This file is git-ignored and contains your EmailJS credentials.
 window.EMAILJS_USER_ID = 'SxH6Bi3FPlFrT6Vbu';
-window.EMAILJS_SERVICE_ID = 'service_bkz7sda';
-window.EMAILJS_TEMPLATE_ID = 'template_jqrv3n2';
+window.EMAILJS_SERVICE_ID = 'service_hhtcb98';
+window.EMAILJS_TEMPLATE_ID = 'template_q76o84i';
